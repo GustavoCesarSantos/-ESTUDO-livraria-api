1cf94ae40f6217ce2d1a083f248f5e608ac2ee90
GustavoCesarSantos <gustavocs789@gmail.com>
2021-10-03 20:19:01 -0300

    feat Adicionado mongodb e criado MongoDbDatabase

---
+++ src/infra/database/MongoDbDatabase.js
@@ -0,0 +1,31 @@
+import mongodb from 'mongodb';
+
+export default class MongoDbDatabase {
+  constructor() {
+    this.instance;
+    this.mongodb = new mongodb.MongoClient(
+      `mongodb+srv://livrariaapi:livrariaapi@cluster0.ecscd.mongodb.net/livraria?retryWrites=true&w=majority`
+    );
+  }
+
+  static getInstance() {
+    if (!MongoDbDatabase.instance)
+      MongoDbDatabase.instance = new MongoDbDatabase();
+    return MongoDbDatabase.instance;
+  }
+
+  async insert(db, collection, data) {
+    await this.mongodb.db(db).collection(collection).insertOne(data);
+  }
+
+  async update(db, collection, query, data) {
+    await this.mongodb
+      .db(db)
+      .collection(collection)
+      .updateOne(query, { $set: { ...data } });
+  }
+
+  async delete(db, collection, query) {
+    await this.mongodb.db(db).collection(collection).deleteOne(query);
+  }
+}
