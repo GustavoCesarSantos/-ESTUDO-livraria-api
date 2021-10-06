3073f15a4424e36848553cf43470b007bb9911b8
GustavoCesarSantos <gustavocs789@gmail.com>
2021-10-06 00:57:58 -0300

    fix: Corrigido bug na conexão e querys do mongo

--- src/infra/database/MongoDbDatabase.js
+++ src/infra/database/MongoDbDatabase.js
@@ -1,9 +1,9 @@
-import mongodb from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 export default class MongoDbDatabase {
   constructor() {
     this.instance;
-    this.mongodb = new mongodb.MongoClient(process.env.MONGO_URL);
+    this.mongodb = new MongoClient(process.env.MONGO_URL);
   }
 
   static getInstance() {
@@ -13,21 +13,44 @@ export default class MongoDbDatabase {
   }
 
   async insert(db, collection, data) {
-    await this.mongodb.db(db).collection(collection).insertOne(data);
+    try {
+      await this.mongodb.connect();
+      await this.mongodb.db(db).collection(collection).insertOne(data);
+    } finally {
+      await this.mongodb.close();
+    }
   }
 
   async update(db, collection, query, data) {
-    await this.mongodb
-      .db(db)
-      .collection(collection)
-      .updateOne(query, { $set: { ...data } });
+    try {
+      await this.mongodb.connect();
+      await this.mongodb
+        .db(db)
+        .collection(collection)
+        .updateOne(query, { $set: { ...data } });
+    } finally {
+      await this.mongodb.close();
+    }
   }
 
   async delete(db, collection, query) {
-    await this.mongodb.db(db).collection(collection).deleteOne(query);
+    try {
+      await this.mongodb.connect();
+      await this.mongodb.db(db).collection(collection).deleteOne(query);
+    } finally {
+      await this.mongodb.close();
+    }
   }
 
   async one(db, collection, query) {
-    await this.mongodb.db(db).collection(collection).findOne(query);
+    try {
+      await this.mongodb.connect();
+      return await this.mongodb
+        .db(db)
+        .collection(collection)
+        .findOne({ $and: [query] });
+    } finally {
+      await this.mongodb.close();
+    }
   }
 }
